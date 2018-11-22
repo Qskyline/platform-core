9e86d9664a6bf317ba9f7b328916706967c1750b
天际 <qrjskyline@gmail.com>
2018-11-22 14:51:27 +0800

    change updateClient.bat

--- updateClient.bat
+++ updateClient.bat
@@ -4,27 +4,28 @@ set build_dir=C:\Users\Administrator\Desktop\clientBuild_temp_dir
 
 set workdir=%~dp0
 set repo_url=https://github.com/Qskyline/client.git
-set prj_name=platform-client
-set version=master
+set prj_name=client
+set branch=master
 
 if exist %build_dir% (
 	cd /d %build_dir%
 	if exist %prj_name% (
 		cd %prj_name%
-		git checkout %version%
-		git pull
 	)else (
 		git clone %repo_url%
 		cd %prj_name%
-		git checkout -b %version% origin/%version%
 	)
 )else (
 	md %build_dir%
 	cd /d %build_dir%
 	git clone %repo_url%
 	cd %prj_name%
-	git checkout -b %version% origin/%version%
 )
+
+set current_time=%date:~,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
+set "current_time=%current_time: =%"
+set build_branch=%branch%-build-%current_time%
+git checkout -b %build_branch% origin/%branch%
 
 call npm install
 call npm run build
@@ -36,4 +37,7 @@ if exist %static_dir% (
 md %static_dir%
 xcopy dist\* %static_dir%\ /e
 
-cd /d %~dp0
+git checkout master
+git branch -d %build_branch%
+
+cd /d %workdir%
