8e1ca77b021bea102f6dcb784ae28b1ec18106ae
天际 <qrjskyline@gmail.com>
2018-11-22 14:35:38 +0800

    change updateClient.sh

--- updateClient.sh
+++ updateClient.sh
@@ -6,27 +6,31 @@ workdir=$(cd `dirname $0`; pwd)
 repo_url=ssh://git@github.com:Qskyline/client.git
 temp=${repo_url##*/}
 prj_name=${temp%.*}
-version=master
+branch=master
 
 if [ -d "$build_dir/$prj_name" ]; then
     cd $build_dir/$prj_name
-    git checkout $version
-    git pull
 elif [ -d "$build_dir" ]; then
     cd $build_dir
     git clone $repo_url
     cd $prj_name
-    git checkout -b $version origin/$version
 else
     mkdir -p $build_dir
     cd $build_dir
     git clone $repo_url
     cd $prj_name
-    git checkout -b $version origin/$version
 fi
+
+current_branch=$(git branch | grep '*' | awk '{print $2}')
+build_branch=$branch"-build-"$(date "+%Y%m%d%H%M")
+git checkout -b $build_branch origin/$branch
 
 npm install
 npm run build
 
 rm -fr $workdir/src/main/resources/static/*
 cp -fr dist/* $workdir/src/main/resources/static/
+
+git checkout $current_branch
+git branch -d $build_branch
+cd $workdir
